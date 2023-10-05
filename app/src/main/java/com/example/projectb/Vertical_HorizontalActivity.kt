ffbbc7f0ef02abdb4fa3a22821c264aeb7f0a2fb
FaqodAqomaddin <115222095+FaqodAqomaddin@users.noreply.github.com>
2023-10-05 22:08:02 +0700

    Add Vertical_Horizontal

--- app/src/main/java/com/example/projectb/Vertical_HorizontalActivity.kt
+++ app/src/main/java/com/example/projectb/Vertical_HorizontalActivity.kt
@@ -1,4 +1,11 @@
 package com.example.projectb
 
-class Vertical_HorizontalActivity {
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Vertical_HorizontalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.vertical_horizontal)
+    }
 }

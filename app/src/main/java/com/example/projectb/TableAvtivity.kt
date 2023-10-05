5ece8f17c16bbf1069a09ee7f92d614e3fdfa94a
FaqodAqomaddin <115222095+FaqodAqomaddin@users.noreply.github.com>
2023-10-05 21:47:58 +0700

    add table layout

---
+++ app/src/main/java/com/example/projectb/TableAvtivity.kt
@@ -0,0 +1,11 @@
+package com.example.projectb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TableAvtivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.table_activity)
+    }
+}
